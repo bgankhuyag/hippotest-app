606b2bcf3524f59cd4ec8ba24ffbdccbb5a57c6d
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-20 13:00:56 +0800

    edit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Questions;
 use App\Models\Answers;
 
@@ -10,7 +11,8 @@ class TestController extends Controller
 {
     public function questions() {
       // dd('here');
-      $questions = Questions::with('answers')->selectRaw('*')->get();
+      // $questions = Questions::with('answers:id,questions_id,answer,correct')->inRandomOrder()->limit(2)->get(['id', 'question']);
+      $questions = Questions::with('answers:id,questions_id,answer,correct')->orderBy(DB::raw('RAND()'))->take(2)->get(['id', 'question']);
       return response()->json($questions);
     }
 }
