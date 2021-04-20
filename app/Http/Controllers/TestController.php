45e37458119834f89598019e9111932219a261b6
Batbold Gankhuyag <bgankhuyag@mail.roanoke.edu>
2021-04-20 15:13:13 +0800

    edit

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,15 +9,24 @@ use App\Models\Answers;
 
 class TestController extends Controller
 {
-    public function questions(Request $request, $category) {
-      // dd($category);
-      $questions = Questions::where('category', $category)->with('answers:id,questions_id,answer,correct')->inRandomOrder()->limit(2)->get(['id', 'question']);
-      // $questions = Questions::where('category', $category)->with('answers:questions_id,answer,correct')->orderBy(DB::raw('RAND()'))->take(2)->get(['id', 'question', 'category']);
-      return response()->json($questions);
-    }
+  // get the questions in specified category
+  public function questions(Request $request, $category) {
+    // dd($category);
+    $questions = Questions::where('category', $category)->with('answers:id,questions_id,answer,correct')->inRandomOrder()->limit(10)->get(['id', 'question']);
+    // $questions = Questions::where('category', $category)->with('answers:questions_id,answer,correct')->orderBy(DB::raw('RAND()'))->take(2)->get(['id', 'question', 'category']);
+    return response()->json($questions);
+  }
 
-    public function categories(Request $request) {
-      $categories = Questions::select('category')->distinct()->get();
-      return response()->json($categories);
-    }
+  // return all distinct categories
+  public function categories(Request $request) {
+    $categories = Questions::select('category')->distinct()->get();
+    return response()->json($categories);
+  }
+
+  // gets the points that the user has scored and add it to the total.
+  public function submit(Request $request) {
+    dd(auth()->user());
+  }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::group([
+    'middleware' => 'api',
+    // 'prefix' => 'auth'
+
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
 Route::post('/questions/{category}', [TestController::class, 'questions'])->name('questions');
 Route::post('/categories', [TestController::class, 'categories'])->name('categories');
+Route::post('/submit', [TestController::class, 'submit']);
